bfac526a5483fff40cf4474f142dfdd5b02dd577
422702 <114009839+422702@users.noreply.github.com>
2022-10-06 13:56:04 +0700

    Коммит

---
+++ Program41.cs
@@ -0,0 +1,29 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine($"Введите массив вещественных чисел:");
+double[] array=CreateArray();
+Console.WriteLine($"Количество чисел больше 0 = {CalculatingCount(array)}");
+
+
+double[] CreateArray()
+{
+    string[] text = Console.ReadLine().Split(" ");
+    double[] array = new double[text.Length];
+    for (int i = 0; i < text.Length; i++)
+    {
+         array[i] = double.Parse(text[i]);
+    }
+    return array;
+}
+
+int CalculatingCount(double[] array)
+{
+    int count = 0;
+        for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ Program43.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+double k1 = EnterNumber("Введите значение k1 ");
+double b1 = EnterNumber("Введите значение b1 ");
+double k2 = EnterNumber("Введите значение k2 ");
+double b2 = EnterNumber("Введите значение b2 ");
+Calculatig(k1,k2,b1,b2);
+
+
+double EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    return double.Parse(Console.ReadLine()!);
+}
+
+void Calculatig(double k1, double k2, double b1, double b2)
+{
+    if(k1==k2)
+    {
+        
+        if(b1==b2)
+        {
+            Console.WriteLine($"Прямые совпадают");
+        }
+        else
+        {
+            Console.WriteLine($"прямые параллельны");
+        }
+    }
+    else
+    {
+        double x=(b2-b1)/(k1-k2);
+        Console.WriteLine($"Точка пересечения двух прямых [{x:f2};{k1 * x + b1:f2}]");
+    }
+}
